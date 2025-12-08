f0d4503f942f05ddbd782b62d8d80da493a0193b
Arun Maity <154771392+Arunmaity2003@users.noreply.github.com>
2025-12-08 23:35:31 +0530

    scroll trigger-scrub,pin

--- gsap2(Scroll Trigger)/script.js
+++ gsap2(Scroll Trigger)/script.js
@@ -1,9 +1,9 @@
-gsap.from("#page1 #box",{
-    scale:0,
-    delay:1,
-    duration:2,
-    rotate:720
-})
+// gsap.from("#page1 #box",{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:720
+// })
 
 // gsap.from("#page2 #box",{
 //     scale:0,
@@ -17,3 +17,46 @@ gsap.from("#page1 #box",{
 //         start:"top 60%"
 //     }
 // })
+
+// gsap.from("#page2 h1",{
+//     opacity:0,
+//     duration:2,
+//     x:500,
+    
+//     scrollTrigger:{
+//         trigger:"#page2 h1",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%"
+//     }
+// })
+
+// gsap.from("#page2 h2",{
+//     opacity:0,
+//     duration:2,
+//     x:-500,
+    
+//     scrollTrigger:{
+//         trigger:"#page2 h2",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%"
+//     }
+// })
+
+gsap.from("#page2 #box",{
+    scale:0,
+    opacity:0,
+    duration:1,
+    rotate:720,
+    
+    scrollTrigger:{
+        trigger:"#page2 #box",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+        end:"top 30%",
+        scrub:3, //use for the smoothness
+        pin:true
+    }
+})

1d3f162976f9b57ac76a95679e4a93d35a7bbebf
Arun Maity <154771392+Arunmaity2003@users.noreply.github.com>
2025-12-14 20:32:19 +0530

    custom cursor

---
+++ custom cursor/script.js
@@ -0,0 +1,29 @@
+let main = document.querySelector("#main")
+let cursor = document.querySelector("#cursor")
+let imageDiv = document.querySelector("#image")
+
+main.addEventListener("mousemove",function(dets){
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y,
+        duration:1.5,
+        
+        // ease:"back.out"
+    })
+})
+
+imageDiv.addEventListener("mouseenter",function(){
+    cursor.innerHTML = "View more"
+    gsap.to(cursor,{
+        scale:4,
+        backgroundColor:"#ffffff5c"
+    })
+})
+
+imageDiv.addEventListener("mouseleave",function(){
+    cursor.innerHTML = ""
+    gsap.to(cursor,{
+        scale:1,
+        backgroundColor:"#fffffff1"
+    })
+})
